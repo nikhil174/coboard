78acb0c803adf07a3486378a3fcc01d0a5ad2ebf
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-02-04 15:12:13 +0530

    add update board mutation funtion

--- convex/board.ts
+++ convex/board.ts
@@ -19,7 +19,7 @@ export const create = mutation({
         orgId: v.string(),
         title: v.string(),
     },
-    handler : async (ctx, args) => {
+    handler: async (ctx, args) => {
         const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
@@ -41,16 +41,41 @@ export const create = mutation({
 })
 
 export const remove = mutation({
-    args: { id: v.id("boards")},
+    args: { id: v.id("boards") },
     handler: async (ctx, args) => {
-      const identity = await ctx.auth.getUserIdentity();
-      
-      if (!identity) {
-        throw new Error("Unauthorized");
-      }
+        const identity = await ctx.auth.getUserIdentity();
 
-      // TODO: Later check to delete favorite relation as well
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
 
-      await ctx.db.delete(args.id);
+        // TODO: Later check to delete favorite relation as well
+
+        await ctx.db.delete(args.id);
+    }
+})
+
+export const update = mutation({
+    args: { id: v.id("boards"), title: v.string() },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const title = args.title.trim();
+        if (!title) {
+            throw new Error("Title is required");
+        }
+
+        if (title.length > 60) {
+            throw new Error("Title cannot be longer than 60 characters")
+        }
+
+        const board = await ctx.db.patch(args.id, {
+            title: args.title
+        });
+
+        return board;
     }
 })
