78ac7cc0fcf8ca5eed096d741fbe505e05746b78
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-02-07 14:47:01 +0530

    update toolbar canvas state functionality

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useSelf } from "@/liveblocks.config";
+import { useCanRedo, useCanUndo, useHistory, useSelf } from "@/liveblocks.config";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
 interface CanvasProps {
     boardId: string;
@@ -12,13 +14,28 @@ interface CanvasProps {
 export const Canvas = ({
     boardId
 }: CanvasProps) => {
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None
+    })
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
+
     return (
         <main
             className="h-full w-full relative bg-nuetral-100 touch-none"
         >
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar />
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo={canRedo}
+                canUndo={canUndo}
+                undo={history.undo}
+                redo={history.redo}
+            />
         </main>
     )
 }
