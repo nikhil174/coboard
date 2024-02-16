9df3be5387baf5c7bfb2573c0348dcbf3bcec618
Nikhil Srivastava <nikhil.faclon@gmail.com>
2024-02-16 14:02:48 +0530

    add ellipse, text, stick note functionality

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -4,6 +4,9 @@ import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Rectangle } from "./rectangle";
+import { Text } from './text';
+import { Ellipse } from "./ellipse";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
     id: string;
@@ -25,6 +28,31 @@ export const LayerPreview = memo(
             return null;
 
         switch (layer.type) {
+            case LayerType.Note:
+                return (
+                    <Note
+                        id={id}
+                        layer={layer}
+                        onPointerDown={onLayerPointerDown}
+                        selectionColor={selectionColor}
+                    />
+                )
+            case LayerType.Text:
+                return (
+                    <Text
+                        id={id}
+                        layer={layer}
+                        onPointerDown={onLayerPointerDown}
+                        selectionColor={selectionColor}
+                    />
+                )
+            case LayerType.Ellipse:
+                return (<Ellipse
+                    id={id}
+                    layer={layer}
+                    onPointerDown={onLayerPointerDown}
+                    selectionColor={selectionColor}
+                />)
             case LayerType.Rectangle:
                 return (
                     <Rectangle
